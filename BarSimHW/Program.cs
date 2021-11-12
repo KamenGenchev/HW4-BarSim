e99b889a0b629374687638bc2252a6ba9e403ded
KamenGenchev <84204800+KamenGenchev@users.noreply.github.com>
2021-11-12 22:19:29 +0200

    Add files via upload

---
+++ BarSimHW/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace BarSimHW
+{
+    class Program
+    {
+       // private static DateTime timeThreadStarted;
+      //  public static List<string> elapsedThreadTime = new List<string>();
+        static void Main(string[] args)
+        {
+            Bar bar = new Bar();
+            Random random = new Random();
+            for (int i = 1; i <= 20; i++)
+            {
+                int rP = random.Next(300, 5000);
+                float price = (float)rP / 100;
+                bar.drinks.Add(new Drinks(i.ToString(), price, random.Next(1, 10)));
+            }
+            
+            List<Thread> studentThreads = new List<Thread>(); 
+            for (int i = 1; i<100; i++)
+            {
+                int age = random.Next(10, 50);
+                int rB = random.Next(1000, 15000);
+                float budget = (float)rB / 100; 
+
+                var students = new Students(i.ToString(), bar, age, budget);
+                var thread = new Thread(students.PaintTheTownRed);
+                thread.Start();
+                studentThreads.Add(thread);
+              //  timeThreadStarted = DateTime.Now;
+               // elapsedThreadTime.Add(new string(timeThreadStarted.ToString()));
+            }
+
+            foreach (var t in studentThreads) t.Join();
+            Console.WriteLine();
+            Console.WriteLine("The party is over.");
+            Console.WriteLine();
+            bar.SaleReport();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BarSimHW/Students.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BarSimHW
+{
+    class Students
+    {
+        enum NightlifeActivities { Walk, VisitBar, GoHome};
+        enum BarActivities { Drink, Dance, Leave};
+
+        Random random = new Random();
+        
+
+        public string Name { get; set; }
+        public Bar Bar { get; set; }
+        public int Age { get; set; }
+        public float Budget { get; set; }
+
+
+        private NightlifeActivities GetRandomNightlifeActivity()
+        {
+            int n = random.Next(10);
+            if (n < 3) return NightlifeActivities.Walk;
+            if (n < 8) return NightlifeActivities.VisitBar;
+            return NightlifeActivities.GoHome;
+        }
+        
+        private BarActivities GetRandomBarActivity()
+        {
+            int n = random.Next(10);
+            if (n < 4) return BarActivities.Dance;
+            if (n < 9) return BarActivities.Drink;
+            return BarActivities.Leave;
+        }
+
+        public void WalkOut()
+        {
+            Console.WriteLine($"{Name} is walking in the streets.");
+            Thread.Sleep(100);
+        }
+
+        private void VisitBar()
+        {
+            if (Bar.BarIsOpen)
+            {
+                Console.WriteLine($"{Name} is getting in line to enter the bar.");
+                
+                if (Bar.Enter(this))
+                {
+                    Console.WriteLine($"{Name} entered the bar!");
+                    bool staysAtBar = true;
+                    while (staysAtBar)
+                    {
+                        var NextActivity = GetRandomBarActivity();
+                        if (Bar.BarIsOpen == false)
+                            NextActivity = BarActivities.Leave;
+
+                        switch (NextActivity)
+                        {
+                            case BarActivities.Dance:
+                                Console.WriteLine($"{Name} dances.");
+                                Thread.Sleep(100);
+                                break;
+                            case BarActivities.Drink:
+                                Bar.BuyDrink(this, Bar.drinks[random.Next(Bar.drinks.Count)]);
+                                Thread.Sleep(100);
+                                break;
+                            case BarActivities.Leave:
+                                Console.WriteLine($"{Name} leaves the bar.");
+                                Bar.Leave(this);
+                                staysAtBar = false;
+                                break;
+                            default: throw new NotImplementedException();
+                        }
+                    }
+                }
+            }
+            else PaintTheTownRed();
+        }
+
+        public void PaintTheTownRed()
+        {
+            WalkOut();
+            bool staysOut = true;
+            while (staysOut)
+            {
+                var NextActivity = GetRandomNightlifeActivity();
+                switch (NextActivity)
+                {
+                    case NightlifeActivities.Walk:
+                        WalkOut();
+                        break;
+                    case NightlifeActivities.VisitBar:
+                        VisitBar();
+                        staysOut = false;
+                        break;
+                    case NightlifeActivities.GoHome:
+                        staysOut = false;
+                        break;
+                    default: throw new NotImplementedException();
+                }
+            }
+            Console.WriteLine($"{Name} is going back home.");
+        }
+
+        public Students(string name, Bar bar, int age, float budget)
+        {
+            Name = name;
+            Bar = bar;
+            Age = age;
+            Budget = budget;
+        }
+    }
+}
